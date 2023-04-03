74598b78584cfdc5cc6099b52b0c697150fe8373
kikikrittamate <kikikrittamate@gmail.com>
2023-04-04 03:17:19 +0700

    add products page details

--- ecommerce/views/products.py
+++ ecommerce/views/products.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from ..models import Item
 
 
+class ProductsView(generic.ListView):
+    template_name = 'ecommerce/products.html'
+    context_object_name = 'items_list'
+
+    def get_queryset(self):
+        return Item.objects.filter().order_by(__name__)
+    
 def products(request):
     return render(request, 'products.html')
