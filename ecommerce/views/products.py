3950625375c537eb0b239d92b231917530d43ae9
kikikrittamate <kikikrittamate@gmail.com>
2023-04-04 13:52:12 +0700

    change products.py

--- ecommerce/views/products.py
+++ ecommerce/views/products.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render
-from django.views import generic
-from ..models import Item
 
 
-class ProductsView(generic.ListView):
-    template_name = 'ecommerce/products.html'
-    context_object_name = 'items_list'
-
-    def get_queryset(self):
-        return Item.objects.filter().order_by(__name__)
-    
-def products(request):
+def products(request, category):
+    # TODO: add query product with category
     return render(request, 'products.html')
