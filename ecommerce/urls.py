8f31e375589e2fd810c875eb3023a1f4d6684851
Natchanon Manatphaiboon <natchanon.space@gmail.com>
2023-04-05 15:00:22 +0700

    add action to for

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -5,5 +5,6 @@ app_name = 'ecommerce'
 urlpatterns = [
     path('', views.home, name='home'),
     path('login', views.login, name='login'),
+    path('products/search', views.search_products, name='search'),
     path('products/<str:category>', views.products, name='products'),
 ]
--- ecommerce/views/products.py
+++ ecommerce/views/products.py
@@ -3,7 +3,13 @@ from ..models import Item, Category
 
 
 def products(request, category):
-    # TODO: add query product with category
     category = Category.objects.get(name=category)
     items_list = Item.objects.filter(category=category)
+    return render(request, 'products.html', { 'items_list': items_list })
+
+
+def search_products(request):
+    q = request.GET.get('products')
+    print("this is request", q)
+    items_list = []
     return render(request, 'products.html', { 'items_list': items_list })
