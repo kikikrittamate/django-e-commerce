f58ca4559623204ec89803ac04b9aa0b5413d34f
Natchanon Manatphaiboon <natchanon.space@gmail.com>
2023-04-05 14:03:16 +0700

    product by category logics

--- ecommerce/views/products.py
+++ ecommerce/views/products.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from ..models import Item, Category
 
 
 def products(request, category):
     # TODO: add query product with category
-    return render(request, 'products.html')
+    category = Category.objects.get(name=category)
+    items_list = Item.objects.filter(category=category)
+    return render(request, 'products.html', { 'items_list': items_list })
