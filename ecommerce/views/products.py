fe17d3a593d6c00454c1804020d8c32912e95660
kikikrittamate <kikikrittamate@gmail.com>
2023-04-05 18:38:51 +0700

    add category text and finish card decorating

--- ecommerce/views/products.py
+++ ecommerce/views/products.py
@@ -6,4 +6,4 @@ def products(request, category):
     # TODO: add query product with category
     category = Category.objects.get(name=category)
     items_list = Item.objects.filter(category=category)
-    return render(request, 'products.html', { 'items_list': items_list })
+    return render(request, 'products.html', { 'items_list': items_list, 'category':category })
