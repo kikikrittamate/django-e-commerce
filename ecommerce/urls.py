1b582d832c25f004b4c4c29f8bd5d6d25944b4e3
Natchanon Manatphaiboon <natchanon.space@gmail.com>
2023-04-06 22:49:52 +0700

    add login page

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -4,9 +4,15 @@ from . import views
 app_name = 'ecommerce'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login', views.login, name='login'),
+    # customer
+    path('login', views.login_customer, name='login'),
+    path('login/submit', views.login_customer_submit, name='login-submit'),
+    path('logout', views.logout_customer, name='logout'),
+    path('register', views.register_custemer, name='register'),
+    # products
     path('products/search', views.search_products, name='search'),
     path('products/<str:category>', views.products, name='products'),
     path('product-detail/<int:product_id>', views.product_detail, name='product-detail'),
+    # shop
     path('shop/<int:shop_id>', views.shop, name="shop"),
 ]
---
+++ ecommerce/views/auth.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from ecommerce.models import User, Customer, Shop
+
+
+def login_customer(request):
+    return render(request, "login.html")
+
+
+def login_customer_submit(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(request, username=username, password=password)
+    print(request.POST.get('username'))
+    if user is not None:
+        login(request, user)
+        return redirect('ecommerce:home')
+    return render(request, "login.html")
+
+
+def logout_customer(request):
+    logout(request)
+    return redirect('ecommerce:home')
+
+
+def register_custemer(request):
+    return render(request, "register.html") 
