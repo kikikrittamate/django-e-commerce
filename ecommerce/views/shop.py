f4ff65b5aca92b4f63034a861ace7a390b2987e5
kikikrittamate <kikikrittamate@gmail.com>
2023-04-07 20:18:33 +0700

    adjust some css and finish shop page

--- ecommerce/views/shop.py
+++ ecommerce/views/shop.py
@@ -4,5 +4,6 @@ from ..models import Shop, Item
 
 def shop(request, shop_id):
     shop = Shop.objects.get(owner_id=shop_id)
+    items = Item.objects.filter(shop=shop)
     print(shop.name)
-    return render(request, 'shop.html', { 'shop': shop} )
+    return render(request, 'shop.html', { 'shop': shop, 'items': items} )
