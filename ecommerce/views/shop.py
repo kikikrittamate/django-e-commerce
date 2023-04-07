0cc0c3373404568e34b8965ad879f9d40698bab1
Natchanon Manatphaiboon <natchanon.space@gmail.com>
2023-04-07 13:46:25 +0700

    deny access from everyone except for shop owner in shop profile page

--- ecommerce/views/shop.py
+++ ecommerce/views/shop.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 from ..models import Shop, Item
-
 
 def shop(request, shop_id):
     shop = Shop.objects.get(owner_id=shop_id)
@@ -8,9 +9,12 @@ def shop(request, shop_id):
     return render(request, 'shop.html', { 'shop': shop } )
 
 
+@login_required(login_url="/shop/login")
 def shop_profile(request, shop_id):
-    # TODO: replace this with shop profile page logics
     shop = Shop.objects.get(owner_id=shop_id)
-    # TODO: deny access from everyone except for owner
-    print(shop.name)
+    # deny access from everyone except for owner
+    if shop.owner_id != request.user.id:
+        return render(request, 'error/403.html', status=403)
+    
+    # TODO: add shop profile logic
     return render(request, 'shop-profile.html', { 'shop': shop } )
