bb9f528284cdf9daff649eb2daee1666ffdc4c3c
Natchanon Manatphaiboon <natchanon.space@gmail.com>
2023-04-08 16:43:43 +0700

    customer registration

--- ecommerce/views/auth.py
+++ ecommerce/views/auth.py
@@ -28,8 +28,42 @@ def register_custemer(request):
 
 
 def register_customer_submit(request):
-    # TODO: add register logic
-    print('submit')
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    cf_password = request.POST.get('cf-password')
+    firstname = request.POST.get('firstname')
+    lastname = request.POST.get('lastname')
+    email = request.POST.get('email')
+    phone = request.POST.get('lastname')
+    address = request.POST.get('address')
+    city = request.POST.get('city')
+    state = request.POST.get('state')
+    country = request.POST.get('country')
+    zipcode = request.POST.get('zipcode')
+    # validate
+    if not validate_username(username):
+        return render(request, "register.html", {"msg": "Username already exist"})
+    if not validate_password(password, cf_password):
+        return render(request, "register.html", {"msg": "Invalid password or confirm password"})
+    # create base user model
+    user = User.objects.create(
+        username=username,
+        email=email,
+        first_name=firstname,
+        last_name=lastname
+    )
+    user.set_password(password)
+    user.save()
+    # create customer instance
+    customer = Customer.objects.create(
+        user=user,
+        address=address,
+        city=city,
+        state=state,
+        country=country,
+        zipcode=zipcode,
+        phone=phone
+    )
     return render(request, "login.html", {"msg": "Completed registration"})
 
 
@@ -48,3 +82,14 @@ def login_shop_submit(request):
         return redirect(reverse("ecommerce:shop-profile", kwargs={"shop_id": shop.owner_id}))
     return render(request, "login-shop.html", {"msg": "Shop does not exist"})
     
+
+def validate_username(username):
+    try:
+        _ = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return True
+    return False
+
+
+def validate_password(password, cf_password):
+    return password == cf_password
