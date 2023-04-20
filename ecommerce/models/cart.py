aa02b606955bb39b29bb24c745bed7372cde3784
kikikrittamate <kikikrittamate@gmail.com>
2023-04-20 19:24:23 +0700

    add model cart

---
+++ ecommerce/models/cart.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib import admin
+from .shop import Shop
+from .item import Item
+from .customer import Customer
+import uuid
+
+class Cart(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='carts')
+    is_ordered = models.BooleanField(default=False)
+
+class CartItems(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_items')
+    item = models.ForeignKey(Item, on_delete=models.SETNULL, null=True, blank=True)
