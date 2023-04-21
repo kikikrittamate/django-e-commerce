3dd5b57f1d7babfed18307d3409df13445aabdbd
kikikrittamate <kikikrittamate@gmail.com>
2023-04-21 19:47:59 +0700

    add order tab

--- ecommerce/models/cart.py
+++ ecommerce/models/cart.py
@@ -9,6 +9,20 @@ class Cart(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='carts')
     is_ordered = models.BooleanField(default=False)
 
+    def get_cart_total(self):
+        cart_items = self.cart_items.all()
+        price = []
+        for cart_item in cart_items:
+            price.append(cart_item.item.price)
+        return sum(price)
+
+
+
+
 class CartItems(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_items')
     item = models.ForeignKey(Item, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def get_item_price(self):
+        price = [self.item.price]
+        return price
